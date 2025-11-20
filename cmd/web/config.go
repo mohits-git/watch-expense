c4d6df57ee84ad033e053406e6dfedc499255302
mohits-git <mohitkhatri2805@gmail.com>
2025-11-20 04:06:59 +0000

    feat: add Dockerfile and fix minor containerization issues with env

--- cmd/web/config.go
+++ cmd/web/config.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -17,16 +16,13 @@ type Config struct {
 }
 
 func LoadConfig() Config {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	_ = godotenv.Load()
 
 	var config Config
 
 	config.ENVIRONMENT = os.Getenv("ENVIRONMENT")
 	if config.ENVIRONMENT == "" {
-		config.ENVIRONMENT = "development"
+		config.ENVIRONMENT = "production"
 	}
 
 	config.MYSQL_DSN = os.Getenv("MYSQL_DSN")
